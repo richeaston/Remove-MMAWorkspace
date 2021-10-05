f396a923b8f9a55f195fda20f57dd866ab77abe6
Richard Easton <richeaston@users.noreply.github.com>
2021-10-05 12:12:59 +0100

    Update Remove-MMAWorkspace.ps1
    Corrected a typo

--- MMAWorkspace/Remove-MMAWorkspace.ps1
+++ MMAWorkspace/Remove-MMAWorkspace.ps1
@@ -20,6 +20,6 @@ function Remove-MMAWorkspace {
 
 #author: Richard Easton
 #description: unlink a server/s from an azure workspace 
-#usage: remoave-mmaworksapce -workspaceid "acbd1234-0000-1a2b-1234-abc1d2345678" -compuntername [yourserver] 
+#usage: remove-mmaworksapce -workspaceid "acbd1234-0000-1a2b-1234-abc1d2345678" -compuntername [yourserver] 
 #optional: $credentials, use get-credential in the normal way
 #optional: can be used in a foreach loop

cc8640b82603ef577006f4560b67462482cbd042
Richard Easton <richeaston@users.noreply.github.com>
2021-10-05 16:27:40 +0100

    Update Remove-MMAWorkspace.ps1

--- MMAWorkspace/Remove-MMAWorkspace.ps1
+++ MMAWorkspace/Remove-MMAWorkspace.ps1
@@ -1,3 +1,10 @@
+
+#author: Richard Easton
+#description: unlink a server/s from an azure workspace 
+#usage: remove-mmaworksapce -workspaceid "acbd1234-0000-1a2b-1234-abc1d2345678" -computername [yourserver] 
+#optional: $credentials, use get-credential in the normal way
+#optional: can be used in a foreach loop
+
 function Remove-MMAWorkspace {
     param
   (
@@ -18,8 +25,3 @@ function Remove-MMAWorkspace {
     $mma.ReloadConfiguration()
 }
 
-#author: Richard Easton
-#description: unlink a server/s from an azure workspace 
-#usage: remove-mmaworksapce -workspaceid "acbd1234-0000-1a2b-1234-abc1d2345678" -compuntername [yourserver] 
-#optional: $credentials, use get-credential in the normal way
-#optional: can be used in a foreach loop
